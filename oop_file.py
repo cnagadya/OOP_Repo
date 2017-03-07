18257a13899821bcc8c45d7e9b1ffca0ce247f63
Christine R Nagadya <Christine R Nagadya>
2017-03-07 19:07:56 +0000

    to do list of the OOP example

---
+++ oop_file.py
@@ -0,0 +1,5 @@
+""" Illustration of inheritance, encapsulation, polymorphism in OOP using a real world example"""
+#inheritance:sub class inheriting from super class
+#encapsulation: privatising a function or variable to disable modification outside the class
+#polymorphism: different forms of the same object type
+
